216c4965d4272bd5379fb5533e90738c787947fc
MagnarMarkus <magnar-markus.lill@tktk.ee>
2024-05-21 13:17:20 +0300

    add week 4 code

---
+++ Week4Loops/Program.cs
@@ -0,0 +1,9 @@
+﻿//FOR-LOOP 
+//DRV-principle - Do Not Repeat Yourself
+int i = 0;
+for (i = 0; i < 5; i++) //i++ = i + 1 ... i = 0(start) i<5 (end) i++ (step)
+{
+    Console.WriteLine("We dont need no education");
+}
+Console.WriteLine(i);
+Console.WriteLine("Have a nice day");
